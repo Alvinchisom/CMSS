528753cb2aaccbcfaed3c7e3ce3e620effcf17a2
Alvinchisom <145863920+Alvinchisom@users.noreply.github.com>
2025-04-02 15:51:25 +0100

    listview

--- cms/blog/url.py
+++ cms/blog/url.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from .views import *
 
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('',Home.as_view(),name='home'),
+    path('article/<int:pk>/',Article.as_view(),name='article_page'),
 ]
--- cms/blog/views.py
+++ cms/blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView,DetailView
+from .models import Post
 # Create your views here.
 
-def home(request):
-    context = {}
-    return render(request,'home.html',context)
+# def home(request):
+#     context = {}
+#     return render(request,'home.html',context)
+
+class Home(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class Article(DetailView):
+    model = Post
+    template_name = 'article.html'
