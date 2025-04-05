7b8776748398cc774e4d0b8e24b7a3e9ae88c4ff
Alvinchisom <145863920+Alvinchisom@users.noreply.github.com>
2025-04-05 20:50:11 +0100

    delete

--- cms/blog/url.py
+++ cms/blog/url.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('article/<int:pk>/',Article.as_view(),name='article_page'),
     path('add_post/',AddPost.as_view(),name='add_post'),
     path('update_post/<int:pk>/',UpdatePost.as_view(),name='update_post'),
+    path('delete_post/<int:pk>/',DeletePost.as_view(),name='delete_post'),
 ]
--- cms/blog/views.py
+++ cms/blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView,CreateView,UpdateView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from .models import Post
 from .forms import PostForm,UpdateForm
+from django.urls import reverse_lazy
 # Create your views here.
 
 # def home(request):
@@ -25,4 +26,9 @@ class UpdatePost(UpdateView):
     model = Post
     form_class = UpdateForm
     template_name = 'update_post.html'
+
+class DeletePost(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
 
