ebd731c151039634685acaf5fcdccd009726706c
Alvinchisom <145863920+Alvinchisom@users.noreply.github.com>
2025-04-06 14:49:06 +0100

    date

--- cms/blog/views.py
+++ cms/blog/views.py
@@ -12,6 +12,7 @@ from django.urls import reverse_lazy
 class Home(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-date']
 
 class Article(DetailView):
     model = Post
