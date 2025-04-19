0c0fecd03c2eb738582d97c5054fd865a07c6ec8
Alvinchisom <145863920+Alvinchisom@users.noreply.github.com>
2025-04-19 18:07:12 +0100

    slice

--- cms/blog/models.py
+++ cms/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime,date
+from ckeditor.fields import RichTextField
 # Create your models here.
 
 class Category(models.Model):
@@ -17,7 +18,8 @@ class Post(models.Model):
     title = models.CharField(max_length=200,null=True,blank=True)
     author = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
     category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True,blank=True)
-    body = models.TextField(null=True,blank=True)
+    body = RichTextField(null=True,blank=True)
+    # body = models.TextField(null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User,related_name='post_likes')
 
