cb0d8c55d205464ffad1ff6a2534e810953230bf
Alvinchisom <145863920+Alvinchisom@users.noreply.github.com>
2025-04-21 13:32:03 +0100

    slice

--- cms/members/url.py
+++ cms/members/url.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserRegister
+from .views import UserRegister,UserProfile
 
 
 urlpatterns = [
     path('register/',UserRegister.as_view(),name='register'),
+    path('user_profile/',UserProfile.as_view(),name='user_profile'),
 ]
--- cms/members/views.py
+++ cms/members/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from .forms import UserProfileView
 # Create your views here.
 
 class UserRegister(generic.CreateView):
     form_class = UserCreationForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+class UserProfile(generic.UpdateView):
+    model = User
+    form_class = UserProfileView
+    template_name = 'registration/user_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
